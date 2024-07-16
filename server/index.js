8ef3cde2a507d0690b4e6a09f1c2792c64755a4e
David Jeong <davidj6873@gmail.com>
2024-07-16 00:12:19 -0700

    Add cors package to server module

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 
 const port = 3200;
 
 const app = express();
 
+app.use(cors());
+
 app.get('/test', (req, res) => {
-    res.send({ data : "Hello from server"});
+    res.send({ 
+        data : "This is the data, with cors now"
+    });
 });
 
 app.listen(port, () => {
