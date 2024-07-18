cda112295cc935ce0f32a1c15f4fdc7ffd310e56
David Jeong <davidj6873@gmail.com>
2024-07-17 22:20:58 -0700

    Display scraped coordinates

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,52 @@
 import React from "react";
 import GoogleMapReact from 'google-map-react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-var heatMapData = {
-  positions: [
-    {lat: 35.0, lng: 127.7},
-    {lat: 35.0, lng: 127.7},
-    {lat: 35.0, lng: 127.7},
-    {lat: 35.0, lng: 127.7},
-    {lat: 41.3163, lng: -72.9223},
-    {lat: 41.3163, lng: -72.9223},
-    {lat: 41.3163, lng: -72.9223},
-    {lat: 41.3163, lng: -72.9223},
-    {lat: 41.3163, lng: -72.9223},
-    {lat: 41.3163, lng: -72.9223},
-    {lat: 41.3163, lng: -72.9223},
-  ],
-  options: {
-    radius: 20,
-    opacity: 0.8
-  }
-}
-
-// onMapClick({x, y, lat, lng, event}) {
-//   if (this._googleMap !== undefined) {
-//     const point = new google.maps.LatLng(lat, lng)
-//     this._googleMap.heatmap.data.push(point)
-//   }
-// }
-
 export default function SimpleMap(){
 
+  const [coordinates, setCoordinates] = useState([]);
   const defaultProps = {
-    center: {lat: 41.3163, lng: -72.9223},
-    zoom: 11
+    center: {lat: 39.8283, lng: -98.5795},
+    zoom: 4
   };
 
-  // // test using index.js
-  // useEffect(() => {
-  //   fetch('http://localhost:3200/test')
-  //     .then(response => response.json())
-  //     .then(data => console.log(data))
-  //     .catch(error => console.error('Error fetching data:', error));
-  // }, []);
-
-  // test parsing
   useEffect(() => {
-    fetch('http://localhost:3200/test')
+    fetch('/test')
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => {
+        setCoordinates(data);
+      })
       .catch(error => console.error('Error fetching coordinates:', error));
   }, []);
 
+  var heatMapData = {
+    positions: coordinates,
+    options: {
+      radius: 10,
+      opacity: 0.8
+    }
+  }
 
 return (
   <div>
     <h1>Header 1</h1>
+    <div>
+      {coordinates.length > 0 ? (
+        <ul>
+          {coordinates.map((coord, index) => (
+            <li key={index}>
+              Latitude: {coord.lat}, Longitude: {coord.lng}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No coordinates available</p>
+      )}
+    </div>
+
     <div className="googleMap">
       <GoogleMapReact
         bootstrapURLKeys={{ 
@@ -69,13 +57,12 @@ return (
         defaultZoom={defaultProps.zoom}
         heatmapLibrary={true}
         heatmap={heatMapData}
-        // onClick={this.onMapClick.bind(this)}
       >
-        <AnyReactComponent
+        {/* <AnyReactComponent
           lat={40.762312}
           lng={-73.979345}
           text="My Marker"
-        />
+        /> */}
       </GoogleMapReact>
     </div>
   </div>
