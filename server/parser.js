245f17c769eff54b20c9d7c098e663a69062f99b
David Jeong <davidj6873@gmail.com>
2024-07-17 22:20:09 -0700

    Add cors to server

--- server/parser.js
+++ server/parser.js
@@ -1,15 +1,17 @@
 const express = require('express');
-//const cors  =require('cors');
+const cors  =require('cors');
 const fs = require('fs');
 const xml2js = require('xml2js');
 
 const app = express();
-//app.use(cors());
+app.use(cors());
 const port = 3200;
 
 // Function to read and parse GPX file
 const parseGpxFile = (filePath, callback) => {
+
   fs.readFile(filePath, (err, data) => {
+
     if (err) throw err;
     xml2js.parseString(data, (err, result) => {
       if (err) throw err;
@@ -18,12 +20,13 @@ const parseGpxFile = (filePath, callback) => {
         lat: trkpt.$.lat,
         lng: trkpt.$.lon
       }));
+      
       callback(coordinates);
     });
   });
 };
 
-// API endpoint to get coordinates
+
 app.get('/test', (req, res) => {
   parseGpxFile('../example.gpx', coordinates => {
     res.json(coordinates);
