d27d4d5d65139651809fa6befbe29a61be80f9f8
David Jeong <davidj6873@gmail.com>
2024-08-07 19:32:30 -0700

    Basic styling of main page

--- client/src/App.js
+++ client/src/App.js
@@ -31,23 +31,40 @@ export default function SimpleMap(){
   }
 
 return (
-  <div>
-    <h1 className='text-6xl'>Header 1</h1>
-    <div>
-      {coordinates.length > 0 ? (
-        <ul>
-          {coordinates.map((coord, index) => (
-            <li key={index}>
-              Latitude: {coord.lat}, Longitude: {coord.lng}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No coordinates available</p>
-      )}
+  <div className="flex h-screen">
+    <div className="w-1/3 border-r p-3">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-sans text-center">
+        HeatMap Visualizer
+      </h1>
+      {/* <div>
+        {coordinates.length > 0 ? (
+          <ul>
+            {coordinates.map((coord, index) => (
+              <li key={index}>
+                Latitude: {coord.lat}, Longitude: {coord.lng}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No coordinates available</p>
+        )}
+      </div> */}
+      <p className="text-xl text-left pt-6 pl-4">
+        A heatmap visualizing tool to allow users to quickly visualize data.<br></br>
+        <span className="block mb-2"></span>
+        Currently parses through GPX files and displays points, but functionality for parsing Google Timeline data is currently being worked on
+      </p>
+
+      <span className="block mb-20"></span>
+
+      <div className="flex items-center justify-center">
+        <button className="p-3 bg-blue-500 text-white rounded shadow-lg hover:bg-blue-600 transition-colors duration-300">
+          Click here to upload a gpx file
+        </button>
+      </div>
     </div>
 
-    <div className="googleMap">
+    <div className="w-2/3 h-full border-l border-gray-500" style={{ borderLeftWidth: '5px' }}>
       <GoogleMapReact
         bootstrapURLKeys={{ 
           key: process.env.REACT_APP_GOOGLEMAPS_API_KEY,
